25001a21c56d149175938ac47955ce3059ba631f
Guilherme Britto <guibrittotava@gmail.com>
2025-09-06 13:31:12 -0300

    feat: adiciona todas as validacoes basicas

--- src/shared/domain/entities/CmSimulation.py
+++ src/shared/domain/entities/CmSimulation.py
@@ -1,4 +1,4 @@
-
+from src.shared.helpers.errors.domain_errors import EntityError
 
 class CmSimulation:
     #geometria
@@ -29,7 +29,9 @@ class CmSimulation:
                  cm_ac: float, 
                  cl_0: float, 
                  cl_alpha: float):
-      pass
+        if not CmSimulation.validate_xcg(xcg):
+          raise EntityError("xcg")
+        self.xcg = xcg
 
     @staticmethod
     def validate_xcg(xcg: float) -> bool:
@@ -40,3 +42,114 @@ class CmSimulation:
         elif xcg < -1 or xcg > 1:
             return False
         return True
+    
+    @staticmethod
+    def validate_xac_w(xac_w: float) -> bool:
+        if xac_w is None:
+            return False
+        elif type(xac_w) != float:
+            return False
+        elif xac_w < 0 or xac_w > 1:
+            return False
+        return True
+    
+    @staticmethod
+    def validate_sw(sw: float) -> bool:
+        if sw is None:
+            return False
+        elif type(sw) != float:
+            return False
+        elif sw <= 0:
+            return False
+        return True
+    
+    @staticmethod
+    def validate_st(st: float) -> bool:
+        if st is None:
+            return False
+        elif type(st) != float:
+            return False
+        elif st <= 0:
+            return False
+        return True
+    
+    @staticmethod
+    def validate_cw(cw: float) -> bool:
+        if cw is None:
+            return False
+        elif type(cw) != float:
+            return False
+        elif cw <= 0:
+            return False
+        return True
+    
+    @staticmethod
+    def validate_ct(ct: float) -> bool:
+        if ct is None:
+            return False
+        elif type(ct) != float:
+            return False
+        elif ct <= 0:
+            return False
+        return True
+    
+    @staticmethod
+    def validate_iw(iw: float) -> bool:
+        if iw is None:
+            return False
+        elif type(iw) != float:
+            return False
+        elif iw < -10 or iw > 10:
+            return False
+        return True
+    
+    @staticmethod
+    def validate_it(it: float) -> bool:
+        if it is None:
+            return False
+        elif type(it) != float:
+            return False
+        elif it < -10 or it > 10:
+            return False
+        return True
+    
+    @staticmethod
+    def validate_lt(lt: float) -> bool:
+        if lt is None:
+            return False
+        elif type(lt) != float:
+            return False
+        elif lt <= 0:
+            return False
+        return True
+    
+    @staticmethod
+    def validate_cm_ac(cm_ac: float) -> bool:
+        if cm_ac is None:
+            return False
+        elif type(cm_ac) != float:
+            return False
+        elif cm_ac < -0.5 or cm_ac > 0.5:
+            return False
+        return True
+    
+    @staticmethod
+    def validate_cl_0(cl_0: float) -> bool: 
+        if cl_0 is None:
+            return False
+        elif type(cl_0) != float:
+            return False
+        elif cl_0 < -1 or cl_0 > 1:
+            return False
+        return True
+    
+    @staticmethod
+    def validate_cl_alpha(cl_alpha: float) -> bool: 
+        if cl_alpha is None:
+            return False
+        elif type(cl_alpha) != float:
+            return False
+        elif cl_alpha <= 0 or cl_alpha > 10:
+            return False
+        return True
+    
