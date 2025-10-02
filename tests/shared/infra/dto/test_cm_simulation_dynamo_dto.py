6a23b4c711d2bfc4a912adb4ad458ded1980a32a
Guilherme Britto <guibrittotava@gmail.com>
2025-10-02 20:50:30 -0300

    feat: adiciona teste to entity do dynamoDTO

--- tests/shared/infra/dto/test_cm_simulation_dynamo_dto.py
+++ tests/shared/infra/dto/test_cm_simulation_dynamo_dto.py
@@ -108,4 +108,40 @@ class Test_CMSimulationDynamoDto:
         )
 
         assert cm_simulation_dto == expected_cm_simulation_dto
+
+    def test_to_entity(self):
+        repo = CmSimulationRepositoryMock()
+
+        cm_simulation_dto = CmSimulationDynamoDTO(
+            simulation_id=repo.simulations[0].simulation_id,
+            xcg=repo.simulations[0].xcg,
+            xac_w=repo.simulations[0].xac_w,
+            sw=repo.simulations[0].sw,
+            st=repo.simulations[0].st,
+            cw=repo.simulations[0].cw,
+            ct=repo.simulations[0].ct,
+            iw=repo.simulations[0].iw,
+            it=repo.simulations[0].it,
+            lt=repo.simulations[0].lt,
+            cm_ac=repo.simulations[0].cm_ac,
+            cl_0=repo.simulations[0].cl_0,
+            cl_alpha=repo.simulations[0].cl_alpha
+        )
+        
+        cm_simulation = cm_simulation_dto.to_entity()
+
+        assert cm_simulation.simulation_id == repo.simulations[0].simulation_id
+        assert cm_simulation.xcg == repo.simulations[0].xcg
+        assert cm_simulation.xac_w == repo.simulations[0].xac_w
+        assert cm_simulation.sw == repo.simulations[0].sw
+        assert cm_simulation.st == repo.simulations[0].st
+        assert cm_simulation.cw == repo.simulations[0].cw
+        assert cm_simulation.ct == repo.simulations[0].ct
+        assert cm_simulation.iw == repo.simulations[0].iw
+        assert cm_simulation.it == repo.simulations[0].it
+        assert cm_simulation.lt == repo.simulations[0].lt
+        assert cm_simulation.cm_ac == repo.simulations[0].cm_ac
+        assert cm_simulation.cl_0 == repo.simulations[0].cl_0
+        assert cm_simulation.cl_alpha == repo.simulations[0].cl_alpha
+
         
