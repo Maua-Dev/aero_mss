ee3223a5d63e713f8a36ed8056a4720bafb30958
Guilherme Britto <guibrittotava@gmail.com>
2025-10-02 21:14:52 -0300

    feat: adiciona teste from entity to dynamo

--- tests/shared/infra/dto/test_cm_simulation_dynamo_dto.py
+++ tests/shared/infra/dto/test_cm_simulation_dynamo_dto.py
@@ -67,19 +67,19 @@ class Test_CMSimulationDynamoDto:
         assert cm_simulation_dto.to_dynamo() == expected_dict
 
     def test_from_dynamo(self):
-        dynamo_dict = {'Simulation': {'simulation_id': 'sim_1',
-                                'xcg': 0.25,
-                                'xac_w': 0.2,
+        dynamo_dict = {'Simulation': {'simulation_id': 'simulation_1',
+                                'xcg': 0.5,
+                                'xac_w': 0.5,
                                 'sw': 1.0,
-                                'st': 0.33,
-                                'cw': 0.5,
-                                'ct': 0.33,
-                                'iw': 0.034,
-                                'it': -0.052,
-                                'lt': 0.75,
-                                'cm_ac': -0.27,
-                                'cl_0': 0.743,
-                                'cl_alpha': 3.558,
+                                'st': 1.0,
+                                'cw': 1.0,
+                                'ct': 1.0,
+                                'iw': 1.0,
+                                'it': 1.0,
+                                'lt': 1.0,
+                                'cm_ac': 0.3,
+                                'cl_0': 0.5,
+                                'cl_alpha': 5.0,
                                 'ResponseMetadata': {'RequestId': 'aa6a5e5e-943f-4452-8c1f-4e5441ee6042',
                                             'HTTPStatusCode': 200,
                                             'HTTPHeaders': {'date': 'Fri, 16 Dec 2022 15:40:29 GMT',
@@ -93,19 +93,19 @@ class Test_CMSimulationDynamoDto:
         cm_simulation_dto = CmSimulationDynamoDTO.from_dynamo(cm_simulation_data=dynamo_dict["Simulation"])
 
         expected_cm_simulation_dto = CmSimulationDynamoDTO(
-            simulation_id="sim_1",
-            xcg=0.25,
-            xac_w=0.2,
-            sw=1.0,
-            st=0.33,
-            cw=0.5,
-            ct=0.33,
-            iw=0.034,
-            it=-0.052,
-            lt=0.75,
-            cm_ac=-0.27,
-            cl_0=0.743,
-            cl_alpha=3.558
+            simulation_id="simulation_1",
+            xcg=0.5, 
+            xac_w=0.5, 
+            sw=1.0, 
+            st=1.0, 
+            cw=1.0, 
+            ct=1.0, 
+            iw=1.0, 
+            it=1.0, 
+            lt=1.0, 
+            cm_ac=0.3, 
+            cl_0=0.5, 
+            cl_alpha=5.0
         )
 
         assert cm_simulation_dto == expected_cm_simulation_dto
@@ -146,19 +146,19 @@ class Test_CMSimulationDynamoDto:
         assert cm_simulation.cl_alpha == repo.simulations[0].cl_alpha
 
     def test_from_dynamo_to_entity(self):
-        dynamo_simulation = {'Simulation': {'simulation_id': 'sim_1',
-                                'xcg': 0.25,
-                                'xac_w': 0.2,
+        dynamo_simulation = {'Simulation': {'simulation_id': 'simulation_1',
+                                'xcg': 0.5,
+                                'xac_w': 0.5,
                                 'sw': 1.0,
-                                'st': 0.33,
-                                'cw': 0.5,
-                                'ct': 0.33,
-                                'iw': 0.034,
-                                'it': -0.052,
-                                'lt': 0.75,
-                                'cm_ac': -0.27,
-                                'cl_0': 0.743,
-                                'cl_alpha': 3.558}}
+                                'st': 1.0,
+                                'cw': 1.0,
+                                'ct': 1.0,
+                                'iw': 1.0,
+                                'it': 1.0,
+                                'lt': 1.0,
+                                'cm_ac': 0.3,
+                                'cl_0': 0.5,
+                                'cl_alpha': 5.0}}
         
         cm_simulation_dto = CmSimulationDynamoDTO.from_dynamo(cm_simulation_data=dynamo_simulation["Simulation"])
 
@@ -166,18 +166,18 @@ class Test_CMSimulationDynamoDto:
 
         expected_cm_simulation = CmSimulation(
             simulation_id="simulation_1",
-            xcg=0.25,
-            xac_w=0.2, 
-            sw=1.0,
-            st=0.33,
-            cw=0.5,
-            ct=0.33,
-            iw=0.034,
-            it=-0.052,
-            lt=0.75,
-            cm_ac=-0.27,
-            cl_0=0.743,
-            cl_alpha=3.558
+            xcg=0.5, 
+            xac_w=0.5, 
+            sw=1.0, 
+            st=1.0, 
+            cw=1.0, 
+            ct=1.0, 
+            iw=1.0, 
+            it=1.0, 
+            lt=1.0, 
+            cm_ac=0.3, 
+            cl_0=0.5, 
+            cl_alpha=5.0
         )
 
         assert cm_simulation.simulation_id == expected_cm_simulation.simulation_id
@@ -193,3 +193,29 @@ class Test_CMSimulationDynamoDto:
         assert cm_simulation.cm_ac == expected_cm_simulation.cm_ac
         assert cm_simulation.cl_0 == expected_cm_simulation.cl_0
         assert cm_simulation.cl_alpha == expected_cm_simulation.cl_alpha
+
+    def test_from_entity_to_dynamo(self):
+        repo = CmSimulationRepositoryMock()
+
+        cm_simulation_dto = CmSimulationDynamoDTO.from_entity(cm_simulation=repo.simulations[0])
+
+        dynamo_dict = cm_simulation_dto.to_dynamo()
+
+        expected_dict = {
+            "entity": "cm_simulation",
+            "simulation_id": repo.simulations[0].simulation_id,
+            "xcg": repo.simulations[0].xcg,
+            "xac_w": repo.simulations[0].xac_w,
+            "sw": repo.simulations[0].sw,
+            "st": repo.simulations[0].st,
+            "cw": repo.simulations[0].cw,
+            "ct": repo.simulations[0].ct,
+            "iw": repo.simulations[0].iw,
+            "it": repo.simulations[0].it,
+            "lt": repo.simulations[0].lt,
+            "cm_ac": repo.simulations[0].cm_ac,
+            "cl_0": repo.simulations[0].cl_0,
+            "cl_alpha": repo.simulations[0].cl_alpha
+        }
+
+        assert dynamo_dict == expected_dict
