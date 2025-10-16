5924e80bbd701b6e4c1ec918a41b1404548053dc
Rafael Ruthes <rafaelruthes444@gmail.com>
2025-10-15 21:19:22 -0300

    fix: uppercase errors

--- src/shared/infra/repositories/cm_simulation_repository_dynamo.py
+++ src/shared/infra/repositories/cm_simulation_repository_dynamo.py
@@ -1,11 +1,11 @@
-from src.shared.domain.entities.cm_simulation import CMSimulation
-from src.shared.domain.repositories.cm_simulation_repository_interface import ICMSimulationRepository
+from src.shared.domain.entities.cm_simulation import CmSimulation
+from src.shared.domain.repositories.cm_simulation_repository_interface import ICmSimulationRepository
 from src.shared.environments import Environments
 from src.shared.helpers.errors.usecase_errors import NoItemsFound
-from src.shared.infra.dto.cm_simulation_dynamo_dto import CMSimulationDynamoDTO
+from src.shared.infra.dto.cm_simulation_dynamo_dto import CmSimulationDynamoDTO
 from src.shared.infra.external.dynamo.datasources.dynamo_datasource import DynamoDatasource
 
-class CMSimulationRepositoryDynamo(ICMSimulationRepository):
+class CMSimulationRepositoryDynamo(ICmSimulationRepository):
     @staticmethod
     def partition_key_format(simulation_id: str) -> str:
         return f"cm_simulation#{simulation_id}"
@@ -21,42 +21,42 @@ class CMSimulationRepositoryDynamo(ICMSimulationRepository):
                                        partition_key=Environments.get_envs().dynamo_partition_key,
                                        sort_key=Environments.get_envs().dynamo_sort_key)
 
-    def get_cm_simulation(self, simulation_id: str) -> CMSimulation:
+    def get_cm_simulation(self, simulation_id: str) -> CmSimulation:
         resp = self.dynamo.get_item(partition_key=self.partition_key_format(simulation_id), sort_key=self.sort_key_format(simulation_id))
 
         if resp.get('Item') is None:
             raise NoItemsFound("simulation_id")
 
-        cm_simulation_dto = CMSimulationDynamoDTO.from_dynamo(resp["Item"])
+        cm_simulation_dto = CmSimulationDynamoDTO.from_dynamo(resp["Item"])
         return cm_simulation_dto.to_entity()
     
-    def get_all_cm_simulations(self) -> list[CMSimulation]:
+    def get_all_cm_simulations(self) -> list[CmSimulation]:
         resp = self.dynamo.get_all_items()
         cm_simulations = []
         for item in resp['Items']:
             if item.get("entity") == 'cm_simulation':
-                cm_simulations.append(CMSimulationDynamoDTO.from_dynamo(item).to_entity())
+                cm_simulations.append(CmSimulationDynamoDTO.from_dynamo(item).to_entity())
 
         return cm_simulations
 
-    def create_cm_simulation(self, new_cm_simulation: CMSimulation) -> CMSimulation:
-        cm_simulation_dto = CMSimulationDynamoDTO.from_entity(cm_simulation=new_cm_simulation)
+    def create_cm_simulation(self, new_cm_simulation: CmSimulation) -> CmSimulation:
+        cm_simulation_dto = CmSimulationDynamoDTO.from_entity(cm_simulation=new_cm_simulation)
         resp = self.dynamo.put_item(partition_key=self.partition_key_format(new_cm_simulation.simulation_id),
                                     sort_key=self.sort_key_format(simulation_id=new_cm_simulation.simulation_id), item=cm_simulation_dto.to_dynamo(),
                                     is_decimal=True)
         return new_cm_simulation
     
-    def delete_cm_simulation(self, simulation_id: str) -> CMSimulation:
+    def delete_cm_simulation(self, simulation_id: str) -> CmSimulation:
         resp = self.dynamo.delete_item(partition_key=self.partition_key_format(simulation_id), sort_key=self.sort_key_format(simulation_id))
 
         if "Attributes" not in resp:
             raise NoItemsFound("simulation_id")
 
-        cm_simulation_dto = CMSimulationDynamoDTO.from_dynamo(resp["Attributes"])
+        cm_simulation_dto = CmSimulationDynamoDTO.from_dynamo(resp["Attributes"])
         return cm_simulation_dto.to_entity()
     
-    def update_cm_simulation(self, simulation_id: str, cm_simulation: CMSimulation) -> CMSimulation:
-        cm_simulation_dto = CMSimulationDynamoDTO.from_entity(cm_simulation=cm_simulation)
+    def update_cm_simulation(self, simulation_id: str, cm_simulation: CmSimulation) -> CmSimulation:
+        cm_simulation_dto = CmSimulationDynamoDTO.from_entity(cm_simulation=cm_simulation)
         resp = self.dynamo.put_item(partition_key=self.partition_key_format(simulation_id),
                                     sort_key=self.sort_key_format(simulation_id=simulation_id), item=cm_simulation_dto.to_dynamo(),
                                     is_decimal=True)
