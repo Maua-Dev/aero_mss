31b5c219f743872b732bbee6f993711edf32c5b4
Rafael Ruthes <rafaelruthes444@gmail.com>
2025-10-15 21:12:12 -0300

    feat: adding dynamo routes and tests

---
+++ tests/shared/infra/repositories/test_cm_simulation_repository_dynamo.py
@@ -0,0 +1,89 @@
+import os
+
+import pytest
+from src.shared.infra.repositories.cm_simulation_repository_dynamo import CMSimulationRepositoryDynamo
+from src.shared.infra.repositories.cm_simulation_repository_mock import CmSimulationRepositoryMock
+
+class Test_CMSimulationRepositoryDynamo:
+    @pytest.mark.skip(reason="Needs dynamoDB")
+    def test_create_cm_simulation(self):
+        os.environ["STAGE"] = "TEST"
+
+        cm_simulation_repository = CMSimulationRepositoryDynamo()
+        cm_simulation_repository_mock = CmSimulationRepositoryMock()
+        resp = cm_simulation_repository.create_cm_simulation(cm_simulation_repository_mock.simulations[0])
+
+        assert cm_simulation_repository_mock.simulations[0].simulation_id == resp.simulation_id
+        assert cm_simulation_repository_mock.simulations[0].xcg == resp.xcg
+
+    @pytest.mark.skip(reason="Needs dynamoDB")
+    def test_get_cm_simulation(self):
+        os.environ["STAGE"] = "TEST"
+
+        cm_simulation_repository = CMSimulationRepositoryDynamo()
+        cm_simulation_repository_mock = CmSimulationRepositoryMock()
+        resp = cm_simulation_repository.get_cm_simulation(cm_simulation_repository_mock.simulations[0].simulation_id)
+
+        assert cm_simulation_repository_mock.simulations[0].simulation_id == resp.simulation_id
+        assert cm_simulation_repository_mock.simulations[0].xcg == resp.xcg
+
+    @pytest.mark.skip(reason="Needs dynamoDB")
+    def test_delete_cm_simulation(self):
+        os.environ["STAGE"] = "TEST"
+
+        cm_simulation_repository = CMSimulationRepositoryDynamo()
+        cm_simulation_repository_mock = CmSimulationRepositoryMock()
+        resp = cm_simulation_repository.delete_cm_simulation(cm_simulation_repository_mock.simulations[0].simulation_id)
+
+        assert cm_simulation_repository_mock.simulations[0].simulation_id == resp.simulation_id
+        assert cm_simulation_repository_mock.simulations[0].xcg == resp.xcg
+
+    @pytest.mark.skip(reason="Needs dynamoDB")
+    def test_get_all_cm_simulation(self):
+        os.environ["STAGE"] = "TEST"
+
+        cm_simulation_repository = CMSimulationRepositoryDynamo()
+        cm_simulation_repository_mock = CmSimulationRepositoryMock()
+        resp = cm_simulation_repository.get_all_cm_simulations()
+
+        assert len(cm_simulation_repository_mock.simulations) == len(resp)
+
+    @pytest.mark.skip(reason="Needs dynamoDB")
+    def test_update_cm_simulation(self):
+        os.environ["STAGE"] = "TEST"
+
+        cm_simulation_repository = CMSimulationRepositoryDynamo()
+        cm_simulation_repository_mock = CmSimulationRepositoryMock()
+
+        original_simulation = cm_simulation_repository_mock.simulations[0]
+        updated_simulation = cm_simulation_repository_mock.simulations[0]
+        updated_simulation.xcg = 0.8  
+        
+        resp = cm_simulation_repository.update_cm_simulation(
+            simulation_id=original_simulation.simulation_id, 
+            cm_simulation=updated_simulation
+        )
+
+        assert resp.simulation_id == original_simulation.simulation_id
+        assert resp.xcg == 0.8
+
+    @pytest.mark.skip(reason="Needs dynamoDB")
+    def test_get_cm_simulation_counter(self):
+        os.environ["STAGE"] = "TEST"
+
+        cm_simulation_repository = CMSimulationRepositoryDynamo()
+        resp = cm_simulation_repository.get_cm_simulation_counter()
+
+        assert isinstance(resp, int)
+        assert resp >= 0
+
+    @pytest.mark.skip(reason="Needs dynamoDB")
+    def test_update_cm_simulation_counter(self):
+        os.environ["STAGE"] = "TEST"
+
+        cm_simulation_repository = CMSimulationRepositoryDynamo()
+        initial_counter = cm_simulation_repository.get_cm_simulation_counter()
+        resp = cm_simulation_repository.update_cm_simulation_counter()
+
+        assert isinstance(resp, int)
+        assert resp == initial_counter + 1
