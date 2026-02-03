e77dcc4adb875ef5580a9cd26de57d672f351e87
Leonardo Luiz Seixas Iorio <lseixas.iorio@gmail.com>
2026-02-03 11:32:36 -0300

    fixed float parsing from json

--- src/modules/create_cm_simulation/app/create_cm_simulation_controller.py
+++ src/modules/create_cm_simulation/app/create_cm_simulation_controller.py
@@ -12,97 +12,30 @@ class CreateCmSimulationController:
 
     def __call__(self, request: IRequest) -> IResponse:
         try:
-            xcg = request.data.get('xcg', None)
-            xac_w = request.data.get('xac_w', None)
-            sw = request.data.get('sw', None)
-            st = request.data.get('st', None)
-            cw = request.data.get('cw', None)
-            ct = request.data.get('ct', None)
-            iw = request.data.get('iw', None)
-            it = request.data.get('it', None)
-            lt = request.data.get('lt', None)
-            cm_ac = request.data.get('cm_ac', None)
-            cl_0 = request.data.get('cl_0', None)
-            cl_alpha = request.data.get('cl_alpha', None)
             
-            if xcg is None:
-                raise MissingParameters('xcg')
-            if isinstance(xcg, float) is False:
-                raise WrongTypeParameter('xcg', 'float', type(xcg).__name__)
+            # implementing a new concept called DRY
             
-            if xac_w is None:
-                raise MissingParameters('xac_w')
-            if isinstance(xac_w, float) is False:
-                raise WrongTypeParameter('xac_w', 'float', type(xac_w).__name__)
+            expected_fields = [
+                'xcg', 'xac_w', 'sw', 'st', 'cw', 'ct',
+                'iw', 'it', 'lt',  'cm_ac', 'cl_0', 'cl_alpha'
+            ]
             
-            if sw is None:
-                raise MissingParameters('sw')
-            if isinstance(sw, float) is False:
-                raise WrongTypeParameter('sw', 'float', type(sw).__name__)
+            params = {}
             
-            if st is None:
-                raise MissingParameters('st')
-            if isinstance(st, float) is False:
-                raise WrongTypeParameter('st', 'float', type(st).__name__)
-            
-            if cw is None:
-                raise MissingParameters('cw')
-            if isinstance(cw, float) is False:
-                raise WrongTypeParameter('cw', 'float', type(cw).__name__)
-            
-            if ct is None:
-                raise MissingParameters('ct')
-            if isinstance(ct, float) is False:
-                raise WrongTypeParameter('ct', 'float', type(ct).__name__)
-            
-            if iw is None:
-                raise MissingParameters('iw')
-            if isinstance(iw, float) is False:
-                raise WrongTypeParameter('iw', 'float', type(iw).__name__)
-            
-            if it is None:
-                raise MissingParameters('it')
-            if isinstance(it, float) is False:
-                raise WrongTypeParameter('it', 'float', type(it).__name__)
-            
-            if lt is None:
-                raise MissingParameters('lt')
-            if isinstance(lt, float) is False:
-                raise WrongTypeParameter('lt', 'float', type(lt).__name__)
-            
-            if cm_ac is None:
-                raise MissingParameters('cm_ac')
-            if isinstance(cm_ac, float) is False:
-                raise WrongTypeParameter('cm_ac', 'float', type(cm_ac).__name__)
-            
-            if cl_0 is None:
-                raise MissingParameters('cl_0')
-            if isinstance(cl_0, float) is False:
-                raise WrongTypeParameter('cl_0', 'float', type(cl_0).__name__)
-            
-            if cl_alpha is None:
-                raise MissingParameters('cl_alpha')
-            if isinstance(cl_alpha, float) is False:
-                raise WrongTypeParameter('cl_alpha', 'float', type(cl_alpha).__name__)
-            
-            
-            simulation = self.usecase(
-                xcg=xcg,
-                xac_w=xac_w,
-                sw=sw,
-                st=st, 
-                cw=cw,
-                ct=ct,
-                iw=iw,
-                it=it,
-                lt=lt,
-                cm_ac=cm_ac,
-                cl_0=cl_0,
-                cl_alpha=cl_alpha
-            )
-            
+            for field in expected_fields:
+                val = request.data.get(field)
+                
+                if val is None:
+                    raise MissingParameters(field)
+                
+                try:
+                    params[field] = float(val)
+                except (ValueError, TypeError):
+                    raise WrongTypeParameter(field, 'float', type(val).__name__)
+                
+            simulation = self.usecase(**params)
+        
             viewmodel = CreateCmSimulationViewmodel(simulation)
-            
             return Created(viewmodel.to_dict())
 
         except MissingParameters as err:
--- src/modules/update_cm_simulation/app/update_cm_simulation_controller.py
+++ src/modules/update_cm_simulation/app/update_cm_simulation_controller.py
@@ -14,78 +14,51 @@ class UpdateCmSimulationController:
     def __call__(self, request: IRequest) -> IResponse:
         try:
             simulation_id = request.data.get('simulation_id')
-            new_xcg = request.data.get('new_xcg')
-            new_xac_w = request.data.get('new_xac_w')
-            new_sw = request.data.get('new_sw')
-            new_st = request.data.get('new_st')
-            new_cw = request.data.get('new_cw')
-            new_ct = request.data.get('new_ct')
-            new_iw = request.data.get('new_iw')
-            new_it = request.data.get('new_it')
-            new_lt = request.data.get('new_lt')
-            new_cm_ac = request.data.get('new_cm_ac')
-            new_cl_0 = request.data.get('new_cl_0')
-            new_cl_alpha = request.data.get('new_cl_alpha')
 
             if simulation_id is None:
                 raise MissingParameters('simulation_id')
             if isinstance(simulation_id, str) is False:
                 raise WrongTypeParameter('simulation_id', 'str', type(simulation_id).__name__)
 
-            if new_xcg is not None and isinstance(new_xcg, float) is False:
-                raise WrongTypeParameter('new_xcg', 'float', type(new_xcg).__name__)
+            float_fields = [
+                'new_xcg', 'new_xac_w', 'new_sw', 'new_st', 
+                'new_cw', 'new_ct', 'new_iw', 'new_it', 
+                'new_lt', 'new_cm_ac', 'new_cl_0', 'new_cl_alpha'
+            ]
 
-            if new_xac_w is not None and isinstance(new_xac_w, float) is False:
-                raise WrongTypeParameter('new_xac_w', 'float', type(new_xac_w).__name__)
+            validated_data = {'simulation_id': simulation_id}
 
-            if new_sw is not None and isinstance(new_sw, float) is False:
-                raise WrongTypeParameter('new_sw', 'float', type(new_sw).__name__)
-
-            if new_st is not None and isinstance(new_st, float) is False:
-                raise WrongTypeParameter('new_st', 'float', type(new_st).__name__)
-
-            if new_cw is not None and isinstance(new_cw, float) is False:
-                raise WrongTypeParameter('new_cw', 'float', type(new_cw).__name__)
-
-            if new_ct is not None and isinstance(new_ct, float) is False:
-                raise WrongTypeParameter('new_ct', 'float', type(new_ct).__name__)
-
-            if new_iw is not None and isinstance(new_iw, float) is False:
-                raise WrongTypeParameter('new_iw', 'float', type(new_iw).__name__)
-
-            if new_it is not None and isinstance(new_it, float) is False:
-                raise WrongTypeParameter('new_it', 'float', type(new_it).__name__)
-
-            if new_lt is not None and isinstance(new_lt, float) is False:
-                raise WrongTypeParameter('new_lt', 'float', type(new_lt).__name__)
-
-            if new_cm_ac is not None and isinstance(new_cm_ac, float) is False:
-                raise WrongTypeParameter('new_cm_ac', 'float', type(new_cm_ac).__name__)
-
-            if new_cl_0 is not None and isinstance(new_cl_0, float) is False:
-                raise WrongTypeParameter('new_cl_0', 'float', type(new_cl_0).__name__)
-
-            if new_cl_alpha is not None and isinstance(new_cl_alpha, float) is False:
-                raise WrongTypeParameter('new_cl_alpha', 'float', type(new_cl_alpha).__name__)
+            for field in float_fields:
+                val = request.data.get(field)
+                
+                # Só validamos e convertemos se o campo foi enviado no request
+                if val is not None:
+                    try:
+                        validated_data[field] = float(val)
+                    except (ValueError, TypeError):
+                        raise WrongTypeParameter(field, 'float', type(val).__name__)
+                else:
+                    # Se for None, passamos None para o usecase (ou omitimos)
+                    validated_data[field] = None
 
             simulation = self.UpdateCmSimulationUsecase(
-                simulation_id=simulation_id,
-                new_xcg=new_xcg,
-                new_xac_w=new_xac_w,
-                new_sw=new_sw,
-                new_st=new_st, 
-                new_cw=new_cw,
-                new_ct=new_ct,
-                new_iw=new_iw,
-                new_it=new_it,
-                new_lt=new_lt,
-                new_Cm_ac=new_cm_ac,
-                new_Cl_0=new_cl_0,
-                new_Cl_alpha=new_cl_alpha
+                simulation_id=validated_data['simulation_id'],
+                new_xcg=validated_data['new_xcg'],
+                new_xac_w=validated_data['new_xac_w'],
+                new_sw=validated_data['new_sw'],
+                new_st=validated_data['new_st'],
+                new_cw=validated_data['new_cw'],
+                new_ct=validated_data['new_ct'],
+                new_iw=validated_data['new_iw'],
+                new_it=validated_data['new_it'],
+                new_lt=validated_data['new_lt'],
+                new_Cm_ac=validated_data['new_cm_ac'], # Ajustado para bater com seu usecase original
+                new_Cl_0=validated_data['new_cl_0'],
+                new_Cl_alpha=validated_data['new_cl_alpha']
             )
 
             viewmodel = UpdateCmSimulationViewmodel(cm_simulation=simulation)
-
+            
             return OK(viewmodel.to_dict())
 
         except NoItemsFound as err:
